c3dfef4933b35b25141dbfb392d8db7cadfd84ce
Abeer <78597516+abeer27917@users.noreply.github.com>
2021-02-08 00:11:22 +0530

    Add files via upload

---
+++ Documents/workspace-spring-tool-suite-4-4.9.0.RELEASE/first/src/com/empmgt/Department.java
@@ -0,0 +1,29 @@
+package com.empmgt.entities;
+
+public class Department {
+	
+	private String deptId;
+	private String deptName;
+	
+	public Department(String id, String name) {
+		this.deptId=id;
+		this.deptName=name;
+	}
+	
+	public String getDeptId() {
+		return deptId;
+	}
+	
+	public void setDeptId(String deptId) {
+		this.deptId=deptId;
+	}
+	
+	public String getDeptName() {
+		return deptName;
+	}
+	
+	public void setDeptName(String deptName) {
+		this.deptName=deptName;
+	}
+
+}
